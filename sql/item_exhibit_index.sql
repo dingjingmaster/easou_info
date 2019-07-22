ed67d247d3c597a607a3f08da277171fe77ca914
dingjingmaster <dingjing@live.cn>
2019-07-22 11:15:19 +0800

    建立索引

--- sql/item_exhibit_index.sql
+++ sql/item_exhibit_index.sql
@@ -3,23 +3,14 @@ use item_exhibit;
 -- DROP INDEX index_fee ON item_exhibit;
 -- DROP INDEX index_all ON item_exhibit;
 -- DROP INDEX index_time ON item_exhibit;
--- DROP INDEX index_area ON item_exhibit;
--- DROP INDEX index_user ON item_exhibit;
 -- DROP INDEX index_module ON item_exhibit;
 -- DROP INDEX index_strategy ON item_exhibit;
 
 
--- ALTER TABLE `item_exhibit` ADD INDEX index_app (`app`);
--- ALTER TABLE `item_exhibit` ADD INDEX index_fee (`itemFee`);
--- ALTER TABLE `item_exhibit` ADD INDEX index_area (`areaLevel`);
--- ALTER TABLE `item_exhibit` ADD INDEX index_user (`userLevel`);
-
-ALTER TABLE `item_exhibit` ADD INDEX index_module (`module`);
-
-
--- ALTER TABLE `item_exhibit` ADD INDEX index_time (`timeStamp`);
+CREATE INDEX idx_app ON item_exhibit(app);
+CREATE INDEX idx_fee ON item_exhibit(itemFee);
+CREATE INDEX idx_module ON item_exhibit(module);
 CREATE INDEX idx_time ON item_exhibit(timeStamp);
-
-ALTER TABLE `item_exhibit` ADD INDEX index_strategy (`strategy`);
-ALTER TABLE `item_exhibit` ADD INDEX index_all (`timeStamp`, `app`, `module`, `areaLevel`, `userLevel`, `userNewOld`, `userFee`, `itemFee`, `strategy`, `status`, `view`, `intime`);
+CREATE INDEX idx_strategy ON item_exhibit(strategy);
+CREATE INDEX idx_all ON item_exhibit(`app`, `module`, `areaLevel`, `userLevel`, `userNewOld`, `userFee`, `itemFee`, `strategy`, `status`, `view`, `intime`);
 
